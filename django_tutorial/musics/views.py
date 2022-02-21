1e3ee556f8116dd37f8d2d0f8b73e253bc309755
shyatu <67035613+shyatu@users.noreply.github.com>
2022-02-21 15:23:44 +0800

    Let hello_view() pass musics to hello_django.html

--- django_tutorial/musics/views.py
+++ django_tutorial/musics/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 
+from musics.models import Music
+
 
 # Create your views here.
 def hello_view(request):
-    return render(request, 'hello_django.html', {'data': "Hello Django"})
+    musics = Music.objects.all()
+
+    return render(request, 'hello_django.html', {
+        'data': "Hello Django ",
+        'musics': musics,
+    })
